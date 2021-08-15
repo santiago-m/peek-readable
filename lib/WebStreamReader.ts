16c636b0f6bf2dd509727370e0c1c1c090fa30ae
Borewit <Borewit@users.noreply.github.com>
2021-08-15 11:21:12 +0200

    Support web-streams (WHATWG standard streams)

---
+++ lib/WebStreamReader.ts
@@ -0,0 +1,53 @@
+// @ts-ignore
+import { ReadableStream, ReadableStreamBYOBReader } from 'node:stream/web';
+import { EndOfStreamError } from './EndOfFileStream.js';
+
+export { EndOfStreamError } from './EndOfFileStream.js';
+
+export class WebStreamReader {
+
+  private reader: ReadableStreamBYOBReader;
+
+  /**
+   * Store peeked data
+   * @type {Array}
+   */
+  private peekQueue: Uint8Array[] = [];
+
+  public constructor(private stream: ReadableStream) {
+    this.reader = stream.getReader({ mode: 'byob' }) as ReadableStreamBYOBReader;
+  }
+
+  /**
+   * Read ahead (peek) from stream. Subsequent read or peeks will return the same data
+   * @param buffer - Uint8Array (or Buffer) to store data read from stream in
+   * @param offset - Offset target
+   * @param length - Number of bytes to read
+   * @returns Number of bytes peeked
+   */
+  public async peek(buffer: Uint8Array, offset: number, length: number): Promise<number> {
+    const bytesRead = await this.read(buffer, offset, length);
+    this.peekQueue.push(buffer.subarray(offset, offset + bytesRead)); // Put read data back to peek buffer
+    return bytesRead;
+  }
+
+  /**
+   * Read chunk from stream
+   * @param buffer - Target Uint8Array (or Buffer) to store data read from stream in
+   * @param offset - Offset target
+   * @param length - Number of bytes to read
+   * @returns Number of bytes read
+   */
+  public async read(buffer: Uint8Array, offset: number, length: number): Promise<number> {
+    if (length === 0) {
+      return 0;
+    }
+    const result = await this.reader.read(new Uint8Array(length));
+
+    if (result.done) {
+      throw new EndOfStreamError();
+    }
+    buffer.set(result.value, offset);
+    return length;
+  }
+}
--- test/util.ts
+++ test/util.ts
@@ -1,9 +1,11 @@
 // Utilities for testing
 
 import { Readable } from 'node:stream';
+// @ts-ignore
+import { ReadableStream, ReadableByteStreamController } from 'node:stream/web';
 
 /**
- * A mock readable-stream, using string to read from
+ * A mock Node.js readable-stream, using string to read from
  */
 export class SourceStream extends Readable {
 
@@ -19,4 +21,34 @@ export class SourceStream extends Readable {
     this.push(this.buf);
     this.push(null); // push the EOF-signaling `null` chunk
   }
+}
+
+/**
+ * A mock ReadableStream, using string to read from
+ */
+export class WebSourceStream extends ReadableStream<Uint8Array> {
+
+  constructor(private str: string = '') {
+    super(new WebSource(Buffer.from(str, 'latin1')));
+  }
+
+}
+
+class WebSource {
+
+  public type = 'bytes';
+  public autoAllocateChunkSize = this.buffer.length;
+
+  constructor(private buffer: Uint8Array, ) {
+  }
+
+  start() {
+  }
+
+  pull(controller: ReadableByteStreamController) {
+    controller.enqueue(this.buffer);
+    controller.close();
+  }
+
+  cancel() {};
 }
